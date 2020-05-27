9467246d5d9134123c5686d886560d3150591883
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-05-27 07:45:45 -0600

    test for user story 10

--- spec/features/users/registration_spec.rb
+++ spec/features/users/registration_spec.rb
@@ -18,13 +18,13 @@ RSpec.describe "User can Register" do
     page.has_field?(:confirm_password)
 
     fill_in :name, with: "Test"
-    save_and_open_page
+    fill_in :address, with: "123 West 3rd Street"
     fill_in :city, with: "Sterling"
-    select 'Arizona', from: 'state'
-    fill_in :zip, with: "80202"
+    fill_in :state, with: "Virginia"
+    fill_in :zip, with: "20166"
     fill_in :email, with: "awesome@gmail.com"
     fill_in :password, with: "asdf"
-    fill_in 'Confirm password:', with: "asdf"
+    fill_in 'confirm_password', with: "asdf"
     click_on "Create Account"
     expect(current_path).to eq("/profile")
     expect(page).to have_content("You are now registered and logged in")
