88e052672e1321722c1f16526d46cba9294374a4
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-05-28 08:02:16 -0600

    update to path helpers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
       if @user.save
         flash[:notice] = "You are now registered and logged in"
         session[:user_id] = @user.id
-        redirect_to "/default_user/profile"
+        redirect_to default_user_profile_path
       else
           flash[:error] = @user.errors.full_messages.to_sentence
           render :new
