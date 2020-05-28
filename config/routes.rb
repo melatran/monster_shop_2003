2eecc20950991a2ef653e67ecf788699b4acb70d
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-05-27 21:02:28 -0600

    delete get route for session destroy

--- config/routes.rb
+++ config/routes.rb
@@ -29,7 +29,6 @@ Rails.application.routes.draw do
   get "/login", to: 'sessions#new'
   post "/login", to: 'sessions#create'
 
-  get "/logout", to: 'sessions#destroy'
   delete "/logout", to: 'sessions#destroy'
 
   namespace :default_user do
