9c182405a4e7d63aafc305fcd9682fc5c39a91f7
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-05-27 20:36:29 -0600

    add route for destroying session

--- config/routes.rb
+++ config/routes.rb
@@ -29,6 +29,9 @@ Rails.application.routes.draw do
   get "/login", to: 'sessions#new'
   post "/login", to: 'sessions#create'
 
+  get "/logout", to: 'sessions#destroy'
+  delete "/logout", to: 'sessions#destroy'
+
   namespace :default_user do
     get "/profile", to: 'profile#index'
   end
