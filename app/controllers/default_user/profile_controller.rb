077e29517f2eee522cc70d0b64d2aac9bd191143
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-05-28 14:30:42 -0600

    add update method

--- app/controllers/default_user/profile_controller.rb
+++ app/controllers/default_user/profile_controller.rb
@@ -1,6 +1,26 @@
 class DefaultUser::ProfileController < DefaultUser::BaseController
 
-    def index       
+    def index
     end
 
-end 
+    def edit
+    end
+
+    def update
+      current_user.update(user_params)
+      if current_user.save
+        flash[:notice] = "Your data was updated"
+        redirect_to default_user_profile_path
+      else
+        flash[:error] = current_user.errors.full_messages.to_sentence
+        redirect_to "/default_user/profile/edit"
+      end
+    end
+
+
+    private
+
+    def user_params
+        params.permit(:name, :address, :city, :state, :zip, :email)
+    end
+end
