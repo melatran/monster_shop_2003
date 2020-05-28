06c7a9bfcf367836b9b4512a8eff49efd3093dde
Josh Tukman <josh.tukman@gmail.com>
2020-05-28 08:36:04 -0600

    Test navigation restrictions to admin namespaced routes for merchant

---
+++ spec/features/merchant_user/navigation_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe 'As a default user' do
+    describe 'site navigation' do
+        it 'I am not able to access any path that begins with /merchant, or /admin' do
+
+            employee = User.create(name: "Employee user",
+                                       address: "99 Working Hard Lane",
+                                       city: "Los Angeles",
+                                       state: "California",
+                                       zip: "90210",
+                                       email: "employee_email@gmail.com",
+                                       password: "employee_password",
+                                       role: 1) 
+                                
+            allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(employee)  
+
+            visit '/admin/dashboard' 
+
+            expect(page).to have_content("The page you were looking for doesn't exist.")
+                  
+        end 
+    end 
+end 
