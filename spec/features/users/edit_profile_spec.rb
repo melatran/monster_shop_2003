15cd09156af57477018c537ba538f502753a2101
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-05-28 15:39:51 -0600

    update

---
+++ spec/features/users/edit_profile_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+RSpec.describe "User can edit their profile data" do
+  it "prepopulates a form for the user to update" do
+    default_user = User.create(name: "Natasha Romanoff",
+      address: "890 Fifth Avenue",
+      city: "Manhattan",
+      state: "New York",
+      zip: "10010",
+      email: "spiderqueen@hotmail.com",
+      password: "arrow",
+      role: 0)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(default_user)
+
+      visit default_user_profile_path
+
+      click_on("Edit Profile")
+
+      has_field?("Name", with: "#{default_user.name}")
+      has_field?("Address", with: "#{default_user.address}")
+      has_field?("City", with: "#{default_user.city}")
+      has_field?("State", with: "#{default_user.state}")
+      has_field?("Zip", with: "#{default_user.zip}")
+      has_field?("Email", with: "#{default_user.email}")
+
+      fill_in "Name", with: "Bruce Banner"
+
+      click_on("Submit update")
+
+      expect(current_path).to eq("/default_user/profile")
+      expect(page).to have_content("Your data was updated")
+      expect(page).to have_content("Bruce Banner")
+      expect(page).to_not have_content("Natasha Romanoff")
+      expect(page).to have_content(default_user.address)
+      expect(page).to have_content(default_user.city)
+      expect(page).to have_content(default_user.state)
+      expect(page).to have_content(default_user.zip)
+      expect(page).to have_content(default_user.email)
+      expect(page).to_not have_content(default_user.password)
+  end
+end
