98f276fc68ecd98e53043b15ddacae76aa3774bb
Melanie Tran <melaniexx02@gmail.com>
2020-05-28 18:22:01 -0600

    Clean up test

--- spec/features/items/item_stats_spec.rb
+++ spec/features/items/item_stats_spec.rb
@@ -54,13 +54,3 @@ RSpec.describe "Items Index Stats Section" do
     end
   end
 end
-
-# User Story 18, Items Index Page Statistics
-#
-# As any kind of user on the system
-# When I visit the items index page ("/items")
-# I see an area with statistics:
-# - the top 5 most popular items by quantity purchased, plus the quantity bought
-# - the bottom 5 least popular items, plus the quantity bought
-#
-# "Popularity" is determined by total quantity of that item ordered
