71f831d2cb222832df55b9a14738ea34eb336a98
Melanie Tran <melaniexx02@gmail.com>
2020-05-29 20:54:20 -0600

    Add test to not show My Orders link when there are no orders

--- spec/features/default_user/orders/index_spec.rb
+++ spec/features/default_user/orders/index_spec.rb
@@ -2,7 +2,6 @@ require "rails_helper"
 
 RSpec.describe "Default User's Profile Orders Page" do
  describe "when I visit my profile page" do
-   describe "I can see a link to My Orders" do
      it "shows a link to My Orders when I have placed orders" do
 
        shop = Merchant.create(name: "K-Pop Black Market", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
@@ -21,6 +20,16 @@ RSpec.describe "Default User's Profile Orders Page" do
 
        expect(current_path).to eq("/default_user/profile/orders")
      end
+
+     it "I cannot see a link to My Orders when I haven't placed an order" do
+
+       default_user = User.create(name: "Natasha Romanoff", address: "890 Fifth Avenue", city: "Manhattan", state: "New York", zip: "10010", email: "spiderqueen@hotmail.com", password: "arrow", role: 0)
+
+       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(default_user)
+
+       visit default_user_profile_path
+
+       expect(page).to_not have_content("My Orders")
+     end
    end
- end
 end
