13fc2188094112bde2e0376a981253edf2b0df3d
Melanie Tran <melaniexx02@gmail.com>
2020-05-29 21:21:04 -0600

    Made a one to many relationship between order and users

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,10 @@ class User < ApplicationRecord
                           :zip
 
     validates_presence_of :password, require: true, :on => :create
+    
+    has_many :orders
 
     has_secure_password
-
     enum role: {default: 0, merchant: 1, admin: 2}
 
 end
