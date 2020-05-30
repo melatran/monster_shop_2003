ebe95c25abe1040e9612692c625994caeed01dd6
Melanie Tran <melaniexx02@gmail.com>
2020-05-30 13:13:15 -0600

    Update test with allow_instance_of and set a variable for the date

--- spec/features/default_user/orders/index_spec.rb
+++ spec/features/default_user/orders/index_spec.rb
@@ -45,8 +45,12 @@ RSpec.describe "Default User's Profile Orders Page" do
 
        user = User.create(name: "Natasha Romanoff", address: "890 Fifth Avenue", city: "Manhattan", state: "New York", zip: "10010", email: "spiderqueen@hotmail.com", password: "arrow", role: 0)
 
-       order1 = Order.create(name: 'Natasha Romanoff', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033, user_id: user.id)
+       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+       order1 = Order.create(name: 'Natasha Romanoff', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033, user_id: user.id, created_at: "May 30, 2020 18:54")
        order2 = Order.create(name: 'Natasha Romanoff', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033, user_id: user.id)
+       create1 = order1.created_at.to_formatted_s(:long)
+       update1 = order1.updated_at.to_formatted_s(:long)
 
        ItemOrder.create(item: cardboard, price: cardboard.price, quantity: 2, order_id: order1.id)
        ItemOrder.create(item: cardboard, price: cardboard.price, quantity: 3, order_id: order2.id)
@@ -54,20 +58,19 @@ RSpec.describe "Default User's Profile Orders Page" do
        visit default_user_profile_orders_path
 
        within ".orders-#{order1.id}" do
-         expect(page).to have_link(order1.id)
-         expect(page).to have_content("Date of Purchase:#{order1.created_at}")
-         expect(page).to have_content("Last Updated:#{order1.updated_at}")
+         expect(page).to have_content("Order Number: #{order1.id}")
+         expect(page).to have_link("#{order1.id}")
+         expect(page).to have_content("Order Placed: #{create1}")
+         expect(page).to have_content("Last Updated: #{update1}")
          # expect(page).to have_content("Status:#{order1.status}")
          expect(page).to have_content("Total Quantity: 2")
          expect(page).to have_content("Grand Total: $200")
          expect(page).to_not have_content(order2.id)
        end
 
-       within ".orders-#{order1.id}" do
-         expect(page).to have_link(order2.id)
-         expect(page).to have_content("Date of Purchase:#{order2.created_at}")
-         expect(page).to have_content("Last Updated:#{order2.updated_at}")
-         # expect(page).to have_content("Status:#{order1.status}")
+       within ".orders-#{order2.id}" do
+         expect(page).to have_content("Order Number: #{order2.id}")
+         expect(page).to have_link("#{order2.id}")
          expect(page).to have_content("Total Quantity: 3")
          expect(page).to have_content("Grand Total: $300")
          expect(page).to_not have_content(order1.id)
