3bff685b567188155ff1046a5c054244a074e3db
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-05-31 19:31:53 -0600

    add update method

--- app/controllers/default_user/orders_controller.rb
+++ app/controllers/default_user/orders_controller.rb
@@ -1,6 +1,38 @@
 class DefaultUser::OrdersController < DefaultUser::BaseController
 
   def index
-    @orders = current_user.orders
+    @orders = active_orders
   end
+
+  def show
+    @order = Order.find(params[:order_id])
+  end
+
+  def update
+    order = Order.find(params[:order_id])
+    order.items.each do |item,quantity|
+      order.item_order.update({
+        quantity: 0,
+        price: 0,
+        status: 0
+        })
+    end
+    order.update({
+      status: 3
+      })
+    binding.pry
+    flash[:notice] = "Your order is now cancelled"
+    redirect_to "/default_user/profile"
+  end
+
+  private
+
+  def active_orders
+    current_user.orders.each_with_object([]) do |order,arr|
+      if order.status != "cancelled"
+        arr << order
+      end
+    end
+  end
+
 end
