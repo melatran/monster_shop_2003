0199a2ff2de2c40a55131402ffe8f061dd65c7fe
Melanie Tran <melaniexx02@gmail.com>
2020-06-02 14:13:56 -0600

    Test for merchant activating an item

---
+++ spec/features/merchant_user/items/activate_spec.rb
@@ -0,0 +1,38 @@
+require "rails_helper"
+
+RSpec.describe "Merchant Employee Can Activate Item" do
+  it "when I activate an item, I see a flash message and my item's status is active" do
+    bike_shop = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+
+    employee = bike_shop.users.create(name: "Employee user",
+                               address: "99 Working Hard Lane",
+                               city: "Los Angeles",
+                               state: "California",
+                               zip: "90210",
+                               email: "employee_email@gmail.com",
+                               password: "employee_password",
+                               role: 1)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(employee)
+
+    tire = bike_shop.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12, active?: false)
+
+    helmet = bike_shop.items.create(name: "Brain Keeper", description: "Will keep your brain in one piece!", price: 30, image: "https://green.harvard.edu/sites/green.harvard.edu/files/field-feature/explore_area_image/bikehelmet_0.jpg", inventory: 6, active?: false)
+
+    visit "/merchant/items"
+
+    within ".items-#{tire.id}" do
+      expect(page).to have_content("Inactive")
+      click_link "Activate"
+    end
+
+    expect(current_path).to eq("/merchant/items")
+    expect(page).to have_content("#{tire.name} is now available for sale")
+
+    tire.reload
+    helmet.reload
+
+    expect(tire.active?).to eq(true)
+    expect(helmet.active?).to eq(false)
+  end
+end
