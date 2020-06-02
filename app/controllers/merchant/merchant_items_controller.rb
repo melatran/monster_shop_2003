b40aca3a3c1080121de44db58a6abbed610b11fa
Melanie Tran <melaniexx02@gmail.com>
2020-06-02 14:14:54 -0600

    Add if statement for activate vs deactive in controller

--- app/controllers/merchant/merchant_items_controller.rb
+++ app/controllers/merchant/merchant_items_controller.rb
@@ -1,6 +1,19 @@
 class Merchant::MerchantItemsController < Merchant::BaseController
 
     def index
-        @user = current_user  
+        @user = current_user
     end
-end 
+
+    def update
+      item = Item.find(params[:id])
+
+      if item.active? == true
+        item.update(active?:false)
+        flash[:notice] = "#{item.name} is no longer for sale"
+      else
+        item.update(active?:true)
+        flash[:notice] = "#{item.name} is now available for sale"
+      end
+      redirect_to "/merchant/items"
+    end
+end
