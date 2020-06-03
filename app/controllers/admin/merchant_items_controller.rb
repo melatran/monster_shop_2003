d6b8a8c23e04e387c60740068576bb314b7a0855
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-06-03 17:23:36 -0600

    Update merchant_items_controller.rb
    remove comments

--- app/controllers/admin/merchant_items_controller.rb
+++ app/controllers/admin/merchant_items_controller.rb
@@ -43,18 +43,4 @@ class Admin::MerchantItemsController < Admin::BaseController
       end
     end
 
-
-  # def update
-  #   merchant = Merchant.find(params[:merchant_id])
-  #   item = Item.find(params[:item_id])
-  #   if item.active? == true
-  #     item.update(active?:false)
-  #     flash[:notice] = "#{item.name} is no longer for sale"
-  #   else
-  #     item.update(active?:true)
-  #     flash[:notice] = "#{item.name} is now available for sale"
-  #   end
-  #   redirect_to "/admin/merchants/#{merchant.id}/items"
-  # end
-
 end
