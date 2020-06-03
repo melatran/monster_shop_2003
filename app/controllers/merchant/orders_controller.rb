ed8865754d9cfaf232b49c3ab468ed293e5bfee9
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-06-03 17:27:47 -0600

    Update orders_controller.rb
    delete commented out code

--- app/controllers/merchant/orders_controller.rb
+++ app/controllers/merchant/orders_controller.rb
@@ -22,27 +22,4 @@ class Merchant::OrdersController < Merchant::BaseController
     end
     @final_orders
   end
-
-  # def find_items
-  #   Item.where("merchant_id = #{current_user.merchant_id}")
-  # end
-  #
-  # def find_item_orders
-  #   if find_items.empty?
-  #     find_items.each_with_object([]) do |item, arr|
-  #       item = ItemOrder.where("#{item.id} = item_id")
-  #       if !item.empty?
-  #         arr << item
-  #       end
-  #     end.flatten
-  #   end
-  # end
-  #
-  # def find_ordersz
-  #   if !find_item_orders.nil?
-  #     find_item_orders.map do |item_order|
-  #       Order.find(item_order.order_id)
-  #     end
-  #   end
-  # end
 end
