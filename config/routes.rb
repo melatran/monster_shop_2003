425cf17e5e7794d7288dc2c31ff3f80c57ceded5
Melanie Tran <melaniexx02@gmail.com>
2020-06-04 12:01:04 -0600

    Add routes to admin/users

--- config/routes.rb
+++ config/routes.rb
@@ -68,5 +68,6 @@ Rails.application.routes.draw do
     get '/merchants/:id/items', to: 'merchant_items#index'
     patch '/merchants/:merchant_id/items/:item_id', to: 'merchant_items#update'
     get '/users', to: 'users#index'
+    get '/users/:id', to: 'users#show'
   end
 end
