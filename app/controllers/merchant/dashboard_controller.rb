08ebfa470c3be20964ea56db9eebb64686417ae1
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-06-04 17:09:58 -0600

    update

--- app/controllers/merchant/dashboard_controller.rb
+++ app/controllers/merchant/dashboard_controller.rb
@@ -1,26 +1,9 @@
 class Merchant::DashboardController < Merchant::BaseController
 
   def index
-    @user = current_user 
+    @user = current_user
     @orders = Order.where('orders.status = 1').joins(:items).where('items.merchant_id = ?', @user.merchant.id).distinct
-    # total_for_merchant_items(@orders)
-  end 
   
-  
-  private
-  
-  # def total_for_merchant_items(orders)
-  #   acc = 0
-    
-  #   orders.each do |order|
-      
-  #     x = ItemOrder.select(:price, :quantity).where(‘order_id = 9’).joins(:item).where(‘item.merchant_id = 15’)
-  #     x.each do |y|
-  #       acc += y.price * y.quantity
-        
+  end
 
-  #     end 
-  #   end
-    
-  # end
 end
--- app/controllers/merchant/orders_controller.rb
+++ app/controllers/merchant/orders_controller.rb
@@ -1,11 +1,8 @@
 class Merchant::OrdersController < Merchant::BaseController
-  # def index
-  #   @orders = find_orders
-  # end
+
 
   def show
     @order = Order.find(params[:id])
-    # @item_orders = current_user.merchant.items_from_order(order)
   end
 
   def update
@@ -16,24 +13,4 @@ class Merchant::OrdersController < Merchant::BaseController
     redirect_to "/merchant/orders/#{item_order.order.id}"
     flash[:notice]= "You have fulfilled the item"
   end
-
-  # private
-  #
-  # def find_orders
-  #   @items = Item.where("merchant_id = #{current_user.merchant_id}")
-  #   if !@items.nil?
-  #     @item_orders = @items.each_with_object([]) do |item, arr|
-  #       item = ItemOrder.where("#{item.id} = item_id")
-  #       if !item.empty?
-  #         arr << item
-  #       end
-  #     end.flatten
-  #   end
-  #   if !@item_orders.empty?
-  #       @final_orders = @item_orders.map do |item_order|
-  #       Order.find(item_order.order_id)
-  #     end
-  #   end
-  #   @final_orders
-  # end
 end
